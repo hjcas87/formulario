e001e7b95af42d6ad5294b3b77136f798f7e7790
[Hernan] <castro.hernan5@gmail.com>
2022-01-08 08:41:12 -0300

    nuevo docs

--- src/components/ui/Navbar.js
+++ src/components/ui/Navbar.js
@@ -1,10 +1,29 @@
-import { NavLink, Outlet } from "react-router-dom";
+import { Link, NavLink, Outlet } from "react-router-dom";
 
 export const Navbar = () => {
     console.log(['object'])
     return (
     <>
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark border-1 border-light border-bottom position-fixed w-100 navegacion">
+        <nav className="navegacion">
+        <div className="navbar-container">
+            <Link to="/" >B D N</Link>
+            <div className="menu-links">
+                <NavLink 
+                    className={ ({ isActive }) => 'nav-item nav-link fs-5 ' + (isActive ? 'active' : '') } 
+                    to="album"
+                >
+                    Álbum
+                </NavLink>
+                <NavLink 
+                    className={ ({ isActive }) => 'nav-item nav-link fs-5 ' + (isActive ? 'active' : '') } 
+                    to="simple"
+                >
+                    Simple
+                </NavLink>
+            </div>
+        </div>
+        </nav>
+        {/* <nav className="navbar navbar-expand-lg navbar-dark bg-dark border-1 border-light border-bottom position-fixed w-100 navegacion">
         <div className="navbar-container">
             <a className="navbar-brand fs-1" href="#">B D N</a>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
@@ -27,7 +46,7 @@ export const Navbar = () => {
             </div>
             </div>
         </div>
-        </nav>
+        </nav> */}
     </>
     )
 }
